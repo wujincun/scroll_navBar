197d574858d0790f034c264e1b56b79d00a20969
wujincun <1769453779@qq.com>
2016-09-14 15:35:13 +0800

    js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
  * Created by wujincun on 2016/9/14.
  */
 
+/*
 $(function(){
     $(window).on('scroll',function () {
         var scrollTop = $(document).scrollTop();
@@ -25,4 +26,56 @@ $(function(){
             $menu.find('[href='+ currentId +']').addClass('current')
         }
     })
-})
+})*/
+window.onload = function () {
+    window.onscroll = function () {
+        var scrollTop = document.documentElement ? document.documentElement.scrollTop : document.body.scrollTop;
+        var menus = document.getElementById('menu').getElementsByTagName('a');
+        var items = getByClassName(document.getElementById('content'),'item');
+        var currentId = "";
+        for(var i=0;i<items.length;i++){
+            var item = items[i];
+            if(item.offsetTop - 200 <scrollTop){
+                currentId = item.id;
+            }else{
+                break
+            }
+        }
+        if(currentId){
+            for(var j=0;j<menus.length;j++){
+                var menu = menus[i];
+                var menuHref = menu.href.split('#');// menu.href : http://hhfhfh#item1
+                if(menuHref[menuHref.length - 1] != currentId){
+                    removeClass(menu,'current')
+                }else{
+                    addClass(menu,'current')
+                }
+            }
+        }
+    }
+};
+function getByClassName(parent,cls) {
+    parent = parent ||document;
+    var childNode = parent.getElementsByTagName('*');
+    var arr = [];
+    for(var i=0;i<childNode.length;i++){
+        if(childNode[i] == cls){
+            arr.push(childNode[i])
+        }
+    }
+    return arr;
+}
+function hasClass(obj,cls) {
+    return obj.className.match(new RegExp("(\\s|^)" +cls + "(\\s|$)"));
+}
+function removeClass(obj,cls) {
+    if(hasClass(obj,cls)){
+        var reg = new RegExp("(\\s|^)" +cls + "(\\s|$)");
+        obj.className = obj.className.replace(reg,"");
+    }
+}
+function addClass(obj,cls) {
+    if(!hasClass(obj,cls)){
+        obj.className += ' ' + cls;
+    }
+}
